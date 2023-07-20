b4a0f6c58d852a76ca72e3931828b86c3db0ae3b
SametSever <139774058+SametSever@users.noreply.github.com>
2023-07-20 13:22:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-sayi = int(input("Sayi giriniz : "))
-asalsayilar = []
+def main():
+    sayi = int(input("Sayi giriniz : "))
+    asalsayilar = []
 
-for i in range(1, sayi):
-    for asal in range(2, i):
-        if i % asal == 0:
-            break
-        if not i % asal == 0 and (asal == i-1):
-            asalsayilar.append(i)
+    for i in range(1, sayi):
+        for asal in range(2, i):
+            if i % asal == 0:
+                break
+            if not i % asal == 0 and (asal == i-1):
+                asalsayilar.append(i)
 
-print(f"{sayi} sayisina kadar bulunan asal sayilar : {asalsayilar}")
+    print(f"{sayi} sayisina kadar bulunan asal sayilar : {asalsayilar}")
+
+if __name__ == "__main__"
+    main()
